ffb6bb7b9dde8a595d0f75d5414cc139837930fa
John Victor Dato-on <143370209+johnvictordatoon@users.noreply.github.com>
2023-12-11 11:45:34 +0800

    Added main menu

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
-class Program {
-    static void Main(string[] args) {
-        Console.WriteLine("Testing");
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\nASEAN PHONEBOOK");
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("[1] Store to ASEAN Phonebook\n[2] Exit");
+            int mainMenu = int.Parse(Console.ReadLine());
+
+            switch (mainMenu)
+            {
+                case 1:
+                    //phonebook.StoreEntry();
+                    //break;
+                case 2:
+                    Console.WriteLine("Exiting...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid number.");
+                    break;
+            }
+        }
     }
 }
